fb78e60edf4263c21644a45a4dfb8fd0ab33ccbd
Primayunita <primayunitaagustine28@gmail.com>
2024-09-05 19:43:47 +0700

    Add files via upload

---
+++ MyFirstJava16.java
@@ -0,0 +1,17 @@
+public class MyFirstJava16 {
+    public static void main(String[] args) {
+       System.out.println("---------------------------------") ;
+       System.out.println("Data Mahasiswa Jurusan Teknologi Informasi POLINEMA");
+       System.out.println("---------------------------------");
+       System.out.println("Nama : Primayunita Putri Agustine");
+       System.out.println("NIM : 244107060094");
+       System.out.println("Kelas : SIB 1A");
+       //System.out.print("\nsaya adalah Mahasiswa Jurusan Teknologi Informasi - POLINEMA");
+       System.out.println("No absen : 16");
+       System.out.println("Prodi : D-IV Sistem Informasi Bisnis");
+       System.out.println("Alamat : Jombang");
+       System.out.println("No HP : 082142568066");
+       System.out.println("Hobi : Membaca");
+
+    }
+}
